3fef9e37831bfd344e6b79d94e70abfeca456b42
Mustafa Hamid Talukdar <68769724+MustafaHamidTalukdar@users.noreply.github.com>
2020-12-28 16:49:04 +0530

    Add files via upload

---
+++ Java_02.java
@@ -0,0 +1,16 @@
+/***********************************
+ ***************@MUSTAFA************
+ **********************************/
+
+package com.company;
+
+public class Java_02 {
+
+    public static void main(String[] args) {
+        float a = 12.5f;
+        float b = 13.3f;
+        float c = 14.2f;
+        float sum = a+b+c;
+        System.out.println(sum);
+    }
+}
